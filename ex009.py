7595cccbd4a5a374193b28ed617a9db99e0da12d
unknown <jonascanuto1@gmail.com>
2025-08-22 00:09:47 -0300

    resolvido exercicio 9 , treinando comando git tambem

--- ex009.py
+++ ex009.py
@@ -1,1 +1,12 @@
 # escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+medida = int (input('digite uma medida em metros, que vou converter em Centimetros e milimetros'))
+cm = medida * 100
+mm = medida *1000
+print (f'{medida} metros em centimetros é igual a: {cm} centimetros' )
+print (f'{medida} metros em milimetros é igual a: {mm} milimetros')
+
+       
+       
+       
+
+
