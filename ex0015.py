b637917ae79296a430961384aa940f4703f23169
unknown <jonascanuto1@gmail.com>
2025-08-27 02:00:40 -0300

    treinando estruturas condiconais (if, elif, else

--- ex0015.py
+++ ex0015.py
@@ -5,10 +5,29 @@ o preço a pagar sabendo que o carro custa r$ 60.00 POR DIA e r$ 0.15 CENTAVOS P
 
 
 
+
+
 km = float (input('quantos Kms foram rodados ?'))
 diaria = int (input('Quantos dias de aluguel do veiculo ?'))
 cobrar = km * 0.15 + diaria * 60
 print (f'foram rodados {km:.2f} Kms e a locação foi de {diaria} dias, o valor total a ser cobrado é de {cobrar:.2f} R$')
+
+
+pagamento = input('\npara pagamento em dinheiro ou pix voce tera 10% de desconto,\n digite a forma de pagamento (dinheiro,pix ou cartao).').lower()
+
+
+if pagamento.lower() == 'dinheiro':
+    desconto = cobrar * 0.10
+    total = cobrar - desconto
+    print(f'voce ganhou 10% de desconto! valor final: R$ {total:.2f}')
+
+elif pagamento == 'pix':
+    desconto = cobrar * 0.10
+    total = cobrar - desconto
+    print(f'10% de desconto valor final: R$ {total:.2f}')
+
+else:
+    print(f'o valor final é de R$ {cobrar:.2f} (sem desconto)') 
 
 
 
