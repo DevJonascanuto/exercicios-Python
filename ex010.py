9142d241ff079141b19ff8c650926d5b0758e803
unknown <jonascanuto1@gmail.com>
2025-08-26 21:54:58 -0300

    corrigindo erro de sintaxe na linha 7

--- ex010.py
+++ ex010.py
@@ -5,4 +5,3 @@ dinheiro = float (input('digite quanto dinheiro voce tem na carteira, que eu te 
 print (f'voçê tem R$ {dinheiro:.2f} , vou calcular quantos dolares pode voce pode comprar. ')
 dolar = dinheiro / 3.27
 print (f'voce pode comprar {dolar:.2f} dolares com {dinheiro} reais')
-
